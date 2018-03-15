0b85c7fee7fcdc30569aaad7a006082446888e3b
Admin <git@github.com:DenisGladkov/test.git>
2018-03-15 16:57:09 +0200

    editcode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,14 @@
 package com.company;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 public class Main {
-    private int mas[] = new int[11];
-
+    //private static int mas[] = new int[11];
+    private static int mas[]={3, 6,9,56,78,25,1,0,3,7};
     //заполнение массива случайными числами
-    private void mas_create() {
+    /*private void mas_create() {
         for (int i = 0; i < mas.length; i++) {
             mas[i] = (int) (Math.random() * 100);
         }
-    }
+    }*/
 
     //вывод массива
     private void ShowMas(){
@@ -63,75 +61,44 @@ public class Main {
         }
     }
 
-    //quick sort
-    /*private void QuickSort(){
-        int a=
-        for(int i=mas.length-1; i>0; i--){
-            for(int j=0; j<i; j++) {
-                if (mas[j] > mas[5]) {
-                    int tmp = mas[j];
-                    mas[j] = mas[j - 1];
-                    mas[j - 1] = tmp;
-                }
+    //Quick Sort
+    private void QuickSort(int[] mas, int start, int end) {
+        int i = start;
+        int j = end;
+        int opora = mas[start + (end - start) / 2];
+        do {
+            while (mas[i] < opora) ++i;
+            while (mas[j] > opora) --j;
+            if (i <= j) {
+                int tmp = mas[i];
+                mas[i] = mas[j];
+                mas[j] = tmp;
+                i++;
+                j--;
             }
-        }
-    }*/
+        } while (i <= j);
 
-    public class QuickSort {
-
-        public static void quickSort(int[] array, int low, int high) {
-            if (array.length == 0)
-                return;//завершить выполнение если длина массива равна 0
-
-            if (low >= high)
-                return;//завершить выполнение если уже нечего делить
-
-            // выбрать опорный элемент
-            int middle = low + (high - low) / 2;
-            int opora = array[middle];
-
-            // разделить на подмассивы, который больше и меньше опорного элемента
-            int i = low, j = high;
-            while (i <= j) {
-                while (array[i] < opora) {
-                    i++;
-                }
-
-                while (array[j] > opora) {
-                    j--;
-                }
-
-                if (i <= j) {//меняем местами
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                    i++;
-                    j--;
-                }
-            }
-
-            // вызов рекурсии для сортировки левой и правой части
-            if (low < j)
-                quickSort(array, low, j);
-
-            if (high > i)
-                quickSort(array, i, high);
-        }
+        if (start < j) QuickSort(mas, start, j);
+        if (i < end) QuickSort(mas, i, end);
+    }
 
 
         public static void main (String[]args){
             Main a=new Main();
+            int start = 0;
+            int end = mas.length - 1;
 
-            a.mas_create();
+            //a.mas_create();
             a.ShowMas();
             a.sum_mas();
             a.max_mas();
             a.min_mas();
-            a.SortMas();
-            System.out.println("Отсортированный массив по возрастанию: ");
+            //a.SortMas();
+            //System.out.println("Отсортированный массив по возрастанию: ");
             a.ShowMas();
-            a.QuickSort();
+
             System.out.println("Отсортированный массив по QuickSort: ");
+            a.QuickSort(mas, start, end);
             a.ShowMas();
         }
     }
